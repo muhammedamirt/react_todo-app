aa2f6a7c91217366d35a0a5f3c1b7c9ced9a6f06
Muhammed Amir T <muhammedamirt@gmail.com>
2022-11-22 18:48:27 +0530

    Remove task Is pending

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 function App() {
+  let d = new Date();
+  let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  let nd = new Date(utc + (3600000 * +5.5));
+  let ist = nd.toLocaleString();
+
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")
+
   return (
-    <div className="app">
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
-      </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's Monday 🌝 ☕ </h2>
-      </div>
-      <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
-      </div>
-      <div className="todos">
-        <h2 className='taskhead'>Task list</h2>
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
+    <div className="container">
+      <div className="app">
+
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Monday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="🖊️ Add item..." />
+          <i className="fas fa-plus" onClick={() => setTodos(todo.length !== 0 ? [...todos, { id: Date.now(), text: todo, status: false, time: ist }] : [...todos])}></i>
+        </div>
+        <div className="todos">
+          <h2 className='taskhead'>Task list</h2>
+          {
+            todos.length !== 0 ? todos.map((todoVal) =>
+
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    setTodos(todos.filter((obj) => {
+                      if (todoVal.id === obj.id) {
+                        todoVal.status = e.target.checked
+                      }
+                      return obj
+                    }))
+                  }} value={todoVal.status} type="checkbox" name="" id={todoVal.id} />
+                  <p className={todoVal.status ? "success" : "pending"}>{todoVal.text} <br /> <span className='dateTime'>{todoVal.time}</span></p>
+
+                </div>
+                <div className="right">
+                  <i className="fas fa-times" onClick={() => setTodos(todos.filter((obj, index) => {
+                    if (obj.id === todoVal.id) {
+                      console.log(index);
+                      let removeTask = [...todos]
+                      removeTask.splice(index, 1)
+                      console.log(removeTask.length);
+                      setTodos(removeTask)
+                    }
+                    return obj
+                  }))}></i>
+                </div>
+              </div>
+            )
+              : <div className="todo">
+                <div className="left">
+                  <p className='emptyValue'>Task is empty</p>
+                </div>
+              </div>
+          }
         </div>
       </div>
     </div>
